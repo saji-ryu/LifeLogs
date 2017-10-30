1678ca65dcac80051d4e5624f5e21f1d8b52a93c
RyuseiSajiki <ryuseisajiki@UbuntuServer.168.1.4>
2017-10-31 00:52:21 +0900

    post部分修正

--- mongo.js
+++ mongo.js
@@ -3,11 +3,11 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const settings = require('./settings.js');
 
-const url = 'mongodb://+' + settings.dbhost + '/' + settings.db;
+const url = 'mongodb://' + settings.dbhost + '/' + settings.db;
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, mongodb) {
-  //assert.equal(null, err);
+  assert.equal(null, err);
   console.log("Connected correctly to db");
   db = mongodb;
 });
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-let router = express.Router();
+const router = express.Router();
 
 const ObjectID = require('mongodb').ObjectID;
 
-const collection = require( '../mongo' );
-const COLNAME = 'rawdatae';
+const collection = require('../mongo');
+const COLNAME = 'rawdata';
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'LifeLog' }); //.ejsは省略できる　オブジジェクトを渡すこともできるよ
+  console.log(collection);
+  res.render('index', {
+    title: 'LifeLog'
+  }); //.ejsは省略できる　オブジジェクトを渡すこともできるよ
 });
 
-router.post( '/', function ( req, res ) {
-  collection(COLNAME).insertOne( req.body ).then(function(r) {
+router.post('/', function(req, res) {
+  console.log(req.body);
+  //res.send(req.body);
+  collection(COLNAME).insertOne(req.body).then(function(r) {
     res.send(r);
   });
 });
