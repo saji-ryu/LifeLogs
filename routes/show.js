a68a6bc19ddc08fd8c7dfa666a71a3194514cce6
saji-ryu <t15443rs@sfc.keio.ac.jp>
2017-10-31 23:48:12 +0900

    追加

---
+++ routes/show.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const ObjectID = require('mongodb').ObjectID;
+
+const collection = require('../mongo');
+const COLNAME = 'rawdata';
+
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'LifeLog'//#{title}として使う
+  });
+});
+
+
+module.exports = router;
