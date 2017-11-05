7ab9ed047bbaa0049bd4e6cb3a1a65a1e2a978d8
saji-ryu <rsajiki@gmail.com>
2017-11-05 15:59:25 +0900

    削除可能に

--- routes/show.js
+++ routes/show.js
@@ -7,7 +7,7 @@ const collection = require('../mongo');
 const COLNAME = 'rawdata';
 
 router.get('/', function(req, res) {
-  collection(COLNAME).find({},{"_id" : 0}).toArray(function(err, docs) {
+  collection(COLNAME).find().toArray(function(err, docs) {
     res.render('show', {
       msg: docs
     });
@@ -16,10 +16,33 @@ router.get('/', function(req, res) {
 });
 
 router.get('/:word', function(req, res) {
-  collection(COLNAME).find({
-  }).toArray(function(err, docs) {
+  collection(COLNAME).find().toArray(function(err, docs) {
     res.render('show', {
       msg: docs
+    });
+  });
+});
+
+router.get('/edit/:dataId', function(req, res) {
+  let id = ObjectID(req.params.dataId)
+  collection(COLNAME).find({
+    '_id': id
+  }, {
+    _id: 0
+  }).toArray(function(err, docs) {
+    console.log(docs);
+    res.render('edit', {
+      msg: docs
+    });
+  });
+});
+
+router.get('/delete/:dataId', function(req, res) {
+  let id = ObjectID(req.params.dataId)
+  collection(COLNAME).deleteOne({
+    '_id': id
+  }).then(function(r) {
+    res.render('delete', {
     });
   });
 });
