e0a6a228f4e9d586fa5c4850e77930b7c1694b75
saji-ryu <rsajiki@gmail.com>
2018-02-02 12:10:07 +0900

    表示の修正

--- public/js/socket-test.js
+++ public/js/socket-test.js
@@ -1,6 +1,7 @@
 var socket = io();
 console.log('ok');
 socket.on("test", function(data){
+  delete data.istaken;
   console.log(data);
   $('#streamtest').prepend('<li>'+JSON.stringify(data)+'</li>');
 });
