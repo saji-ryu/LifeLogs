dd79fc29332a24e396bbf20bb9aba979955acbde
saji-ryu <rsajiki@gmail.com>
2018-02-03 13:31:46 +0900

    idも非表示で

--- public/js/socket-test.js
+++ public/js/socket-test.js
@@ -2,6 +2,7 @@ var socket = io();
 console.log('ok');
 socket.on("stream", function(data){
   delete data.istaken;
+  delete data._id;
   console.log(data);
-  $('<li>'+JSON.stringify(data)+'</li>').prependTo('#streamtest').hide().fadeIn(800);
+  $('<li>'+JSON.stringify(data)+'</li>').prependTo('#streamtest').hide().fadeIn(500);
 });
